3ef5add6735eadcef354f7bcf5ddcabef79be9db
Mark Francis <markxf@gmail.com>
2019-10-16 17:23:56 +0100

    remove package.json

--- app.js
+++ app.js
@@ -47,14 +47,3 @@ mongoose
   .catch(err => {
     console.log(err);
   });
-
-   if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('frontend/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
